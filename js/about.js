42118a72b5b202ee2e54cb67d23569e6a5d0788c
smallyou0 <whiteroot45811@gmail.com>
2024-07-23 19:01:47 +0900

    upload3

--- js/about.js
+++ js/about.js
@@ -4,4 +4,24 @@ jQuery(document).ready(function ($) {
         time: 1000,
     });
 
+    const goTopBtn = document.getElementById("goTopBtn");
+
+    // 스크롤 이벤트 핸들러
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 300) {
+            goTopBtn.classList.add("show");
+        } else {
+            goTopBtn.classList.remove("show");
+        }
+    });
+
+    // 클릭 이벤트 핸들러
+    goTopBtn.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+
 });
