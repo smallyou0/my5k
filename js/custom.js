827c691518e0c7a9c6d80ebffb53cc0b0fefc5c7
smallyou0 <whiteroot45811@gmail.com>
2024-07-25 19:01:58 +0900

    upload11

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,21 @@
 jQuery(document).ready(function ($) {
-    $('body').prepend('<header></header>');
-    $('body').append('<footer></footer>');
-    $('header').load('./header.html');
-    $('footer').load('./footer.html');
+    var isIndexPage = window.location.pathname === '/';
 
+    if (isIndexPage) {
+        // index.php 페이지일 때 경로 설정
+        $('body').prepend('<header></header>');
+        $('body').append('<footer></footer>');
+        $('header').load('./html/header.html');
+        $('footer').load('./html/footer.html');
+    } else {
+        // 그 외 페이지일 때 경로 설정
+        $('body').prepend('<header></header>');
+        $('body').append('<footer></footer>');
+        $('header').load('./header.html');
+        $('footer').load('./footer.html');
+    }
+
+    
 });
 
 
