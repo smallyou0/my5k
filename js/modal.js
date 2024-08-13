d952d52da11b9020fe9d5dbf7b1f2e0d4edfb10b
smallyou0 <whiteroot45811@gmail.com>
2024-08-13 16:18:01 +0900

    upload25

--- js/modal.js
+++ js/modal.js
@@ -15,3 +15,72 @@ $('.btn-close').click(function() {
     $('.layer').removeClass('active')
     $('#termsModal').removeClass('active')
 })
+// edit-meminfo
+$('.btn-edit').click(function() {
+    $('.layer').addClass('active')
+    $('#alertModal1').addClass('active')
+})
+$('.btn-close').click(function() {
+    $('.layer').removeClass('active')
+    $('#alertModal1').removeClass('active')
+})
+// apply-ticket-list-detail
+// 결제를 취소하시겠습니까?
+$('#cancelPayment').click(function() {
+    $('.layer').addClass('active')
+    $('#alertModal2').addClass('active')
+})
+$('.btn-close').click(function() {
+    $('.layer').removeClass('active')
+    $('#alertModal2').removeClass('active')
+    $('#alertModal3').removeClass('active')
+})
+$('.btn-close2').click(function() {
+    $('#alertModal2').removeClass('active')
+})
+$('#btnConfirm').click(function() {
+    // $('.layer').removeClass('active')
+    $('#alertModal2').removeClass('active')
+    $('#alertModal3').addClass('active')
+})
+// 신청 정보 수정 기간이 지났습니다.
+$('#editInfo').click(function() {
+    $('.layer').addClass('active')
+    $('#alertModal1').addClass('active')
+})
+$('.btn-close').click(function() {
+    $('.layer').removeClass('active')
+    $('#alertModal1').removeClass('active')
+})
+// 참가권 신청 정보 수정이 완료되었습니다.
+$('#editInfo').click(function() {
+    $('.layer').addClass('active')
+    $('#alertModal4').addClass('active')
+})
+$('.btn-close').click(function() {
+    $('.layer').removeClass('active')
+    $('#alertModal4').removeClass('active')
+})
+// apply-ticket-list-edit
+// 참가권 신청 정보 수정이 완료되었습니다.
+$('#editCompleted').click(function() {
+    $('.layer').addClass('active')
+    $('#alertModal1').addClass('active')
+})
+$('.btn-close').click(function() {
+    $('.layer').removeClass('active')
+    // $('#alertModal1').removeClass('active')
+})
+// apply-ticket-present-edit
+// 참가권 신청 정보 수정이 완료되었습니다.
+$('#btnEdit').click(function() {
+    $('.layer').addClass('active')
+    $('#alertModal1').addClass('active')
+})
+$('.btn-close').click(function() {
+    $('.layer').removeClass('active')
+    $('#alertModal1').removeClass('active')
+})
+
+
+
